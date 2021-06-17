315dbd1d617ff9cfb496fcc4bd6644d8343f17e7
xiaolaba <xiao_laba_cn@yahoo.com>
2021-06-17 17:30:34 +0800

    Add files via upload

---
+++ dis.bat
@@ -0,0 +1,8 @@
+
+::https://ucexperiment.wordpress.com/2015/01/04/dump-and-disassemble-avr-%C2%B5c-flash-memory/
+
+set path=%PATH%;C:\avr8-gnu-toolchain-win32_x86\bin
+
+avr-objdump -j .sec1 -d -m avr5 bootice16_rom.hex > bootice16_rom.asm
+
+pause
